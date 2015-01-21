0e7487dff489bba857d74149ccbf03bc23583991
Katja Durrani <mail@kdurrani.co.uk>
2015-01-21 23:35:09 +0000

    change app name in mongo connection

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
+  process.env.OPENSHIFT_NODEJS;
 }
 
 //var db = mongoose.createConnection('localhost', 'pollsapp');
